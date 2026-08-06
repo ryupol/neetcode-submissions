6a570de34275277b7bbb02cafa13b2cbe6cbcab8
Polawat Maniratanapisut <107712915+ryupol@users.noreply.github.com>
2026-08-06 22:56:29 +0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # ["act","pots","tops"]
+        # hashmap = {[0]x26: 0, [0]x26: 1}
+        # res = [["act"], [pots, tops]]
+
+        hashmap = {}
+        res = []
+        i = 0
+        for word in strs:
+            alp_count = [0] * 26
+            for w in word:
+                alp_index = ord(w) - ord("a")
+                alp_count[alp_index] += 1
+
+            alp_count_tuple = tuple(alp_count)
+            
+            if alp_count_tuple in hashmap:
+                res[hashmap[alp_count_tuple]].append(word)
+            else:
+                res.append([word])
+                hashmap[alp_count_tuple] = i
+                i += 1
+        return res
+
