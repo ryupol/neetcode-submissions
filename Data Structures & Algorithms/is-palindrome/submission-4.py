57b4ef112aad38210a82dbe5af64c82c9f7076e1
Polawat Maniratanapisut <107712915+ryupol@users.noreply.github.com>
2026-08-14 02:14:15 +0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        char_set = set(chars)
+
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            while l < len(s) - 1 and s[l] not in char_set:
+                l += 1
+            
+            while r >= 0 and s[r] not in char_set:
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
+                return False
+
+            l += 1
+            r -= 1
+
+        return True
