a326d3268af23080fd172d5d1e611ff3e2f32f53
Polawat Maniratanapisut <107712915+ryupol@users.noreply.github.com>
2026-08-25 23:19:16 +0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev2, prev1 = 0, 0
+
+        for num in nums:
+            temp = max(num + prev2, prev1)
+            prev2 = prev1
+            prev1 = temp
+
+        return max(prev1, prev2)
+            
